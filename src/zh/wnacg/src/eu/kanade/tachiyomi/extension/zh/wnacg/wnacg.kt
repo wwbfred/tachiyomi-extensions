5a45a0686446df461fb8ecbd4483831b6ba9a4c7
Fred Wu <wwbfred@gmail.com>
2023-03-25 07:42:39 +0800

    Update wnacg.kt

--- src/zh/wnacg/src/eu/kanade/tachiyomi/extension/zh/wnacg/wnacg.kt
+++ src/zh/wnacg/src/eu/kanade/tachiyomi/extension/zh/wnacg/wnacg.kt
@@ -18,7 +18,7 @@ import rx.Observable
 // URL can be found at https://www.wnacglink.top/
 class wnacg : ParsedHttpSource() {
     override val name = "紳士漫畫"
-    override val baseUrl = "https://www.htcomic.top"
+    override val baseUrl = "http://www.htmanga3.top"
     override val lang = "zh"
     override val supportsLatest = false
 
